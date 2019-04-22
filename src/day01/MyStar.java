065bd03e1d66ac50bbcf280f5f08f94065fab19d
sorthorange <752893258@qq.com>
2019-04-22 15:33:16 +0800

    github test

---
+++ src/day01/MyStar.java
@@ -0,0 +1,20 @@
+package day01;
+
+import javax.swing.*;
+
+public class MyStar {
+    public static void main(String [] args)
+    {
+        JFrame frame = new JFrame();
+        MyStarPanel panel = new MyStarPanel();
+        frame.add(panel);
+
+        Thread t= new Thread(panel);
+        t.start();
+
+        frame.setSize(800,600);//设置窗口大小
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//设置窗口关闭模式
+        frame.setLocationRelativeTo(null);//设置窗口居中
+        frame.setVisible(true);//设置窗口可见
+    }
+}
---
+++ src/day01/MyStarPanel.java
@@ -0,0 +1,54 @@
+package day01;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyStarPanel extends JPanel implements Runnable{
+
+    int []xx = new int[100];
+    int []yy = new int[100];
+
+    public MyStarPanel()
+    {
+        for(int i=0;i<100;i++)
+        {
+            xx[i]=(int)(Math.random()*800);
+            yy[i]=(int)(Math.random()*600);
+        }
+    }
+
+    @Override
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        this.setBackground(Color.BLACK);
+        Font ft = new Font("微软雅黑",Font.BOLD,28);//设置大小
+        g.setFont(ft);
+        //g.setColor(Color.);
+        for(int i=0;i<100;i++)
+        {
+            //int x=(int)(Math.random()*800);
+            //int y=(int)(Math.random()*600);
+
+            int R=(int)(Math.random()*255);
+            int G=(int)(Math.random()*255);
+            int B=(int)(Math.random()*255);
+            Color randcolor = new Color(R,G,B);
+            g.setColor(randcolor);
+            g.drawString("♂",xx[i],yy[i]);
+        }
+        //g.drawString("*",100,100);
+    }
+
+
+    @Override
+    public void run() {
+        while(true)
+        {
+            //业务执行
+            //睡眠方法
+            //调用重绘方法
+            repaint();
+        }
+    }
+}
